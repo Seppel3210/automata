6e4ae6e0576ec24c39258385a973746050d7ee45
Sebastian Widua <seppel3210@gmail.com>
2021-02-13 13:53:26 +0100

    Implement buggy ReverseAutomaton

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use std::collections::HashMap;
+use std::collections::BTreeMap;
+use std::collections::BTreeSet;
+use std::io::Write;
 
 #[derive(Debug)]
 struct Automaton<'a> {
@@ -42,7 +44,7 @@ impl<'a> Automaton<'a> {
 struct State<'a> {
     /// Mapping from one word to the index of the index of the corresponding
     /// `State`
-    transitions: HashMap<&'a str, usize>,
+    transitions: BTreeMap<&'a str, usize>,
     end_state: bool,
 }
 
@@ -59,6 +61,90 @@ impl<'a> State<'a> {
     }
 }
 
+#[derive(Debug)]
+struct ReverseAutomaton<'a> {
+    states: Vec<ReverseState<'a>>,
+    end_states: Vec<usize>,
+}
+
+#[derive(Debug, Clone)]
+struct ReverseState<'a> {
+    parent_states: Vec<(usize, &'a str)>,
+}
+
+impl<'a> ReverseAutomaton<'a> {
+    fn from_automaton(automaton: &Automaton<'a>) -> Self {
+        let mut states = vec![ReverseState::new(); automaton.states.len()];
+        let mut end_states = vec![];
+        for (state_idx, state) in automaton.states.iter().enumerate() {
+            if state.end_state {
+                end_states.push(state_idx);
+            }
+            for (word, &child) in &state.transitions {
+                states[child].add_parent(state_idx, word);
+            }
+        }
+
+        ReverseAutomaton { states, end_states }
+    }
+
+    fn print_language(&self) {
+        let mut paths: Vec<_> = self.end_states.iter().map(|i| (*i, String::new())).collect();
+        while !paths.is_empty() {
+            let mut idcs_to_remove = BTreeSet::new();
+            for i in 0..paths.len() {
+                let state_idx = paths[i].0;
+                if state_idx == 0 {
+                    idcs_to_remove.insert(i);
+                }
+                let mut iter = self.states[state_idx].parent_states.iter();
+                let first = iter.next();
+                for (parent, word) in iter {
+                    fork_path(&mut paths, i, (*parent, word.to_string()));
+                }
+                if let Some(tail) = first {
+                    let mut tail = (tail.0, tail.1.to_owned());
+                    if i == 0 {
+                        fork_path(&mut paths, i, tail.clone());
+                    }
+                    if !idcs_to_remove.contains(&i) {
+                        tail.1.push_str(&paths[i].1);
+                        paths[i] = tail;
+                    }
+                }
+            }
+            println!("{:?}", paths);
+            println!("{:?}", idcs_to_remove);
+            std::io::stdin().read_line(&mut String::new());
+            for i in idcs_to_remove.iter().rev() {
+                println!("{}", paths.swap_remove(*i).1);
+            }
+        }
+    }
+}
+
+fn fork_path(
+    paths: &mut Vec<(usize, String)>,
+    path_idx: usize,
+    mut new_tail: (usize, String),
+) {
+    let old_tail = &paths[path_idx];
+    new_tail.1.push_str(&old_tail.1);
+    paths.push(new_tail);
+}
+
+impl<'a> ReverseState<'a> {
+    fn new() -> Self {
+        ReverseState {
+            parent_states: vec![],
+        }
+    }
+
+    fn add_parent(&mut self, state_idx: usize, word: &'a str) {
+        self.parent_states.push((state_idx, word));
+    }
+}
+
 fn nums_to_dates<I>(nums: I, next_state: usize) -> impl Iterator<Item = (String, usize)>
 where
     I: Iterator<Item = usize>,
@@ -66,6 +152,7 @@ where
     nums.map(|n| format!("{:02}.", n))
         .map(move |s| (s, next_state))
 }
+
 fn main() {
     let mut auto = Automaton::new();
     let mut s0_trans: Vec<_> = nums_to_dates(1..=28, 1).collect();
@@ -81,10 +168,21 @@ fn main() {
     auto.add_state(State::new(s3_trans.iter().map(|(s, i)| (&**s, *i)), false));
     auto.add_state(State::new(vec![("2006", 5)], false));
     auto.add_state(State::new(vec![], true));
+    let rev_auto = ReverseAutomaton::from_automaton(&auto);
+    //rev_auto.print_language();
+
+    let mut auto = Automaton::new();
+    auto.add_state(State::new(vec![("0", 1)], false));
+    auto.add_state(State::new(vec![("1", 0)], true));
+    let rev_auto = ReverseAutomaton::from_automaton(&auto);
+    println!("{:?}", rev_auto);
+    rev_auto.print_language();
+    ///*
     loop {
         let mut buf = String::new();
         std::io::stdin().read_line(&mut buf).unwrap();
         let success = auto.run(&buf.trim());
         println!("success: {}", success);
     }
+    //*/
 }
